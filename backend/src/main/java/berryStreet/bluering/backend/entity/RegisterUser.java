25a25ba871dee9ff1cb1690fbf07bdae2a4d94a2
guanyujing <guanyujing_i@didiglobal.com>
2021-09-02 20:19:45 +0800

    register

--- backend/src/main/java/berryStreet/bluering/backend/entity/RegisterUser.java
+++ backend/src/main/java/berryStreet/bluering/backend/entity/RegisterUser.java
@@ -1,7 +1,9 @@
 package berryStreet.bluering.backend.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -12,4 +14,64 @@ public class RegisterUser {
     private String password;
     private String role;
     private String inviteCode;
+
+    public RegisterUser(int UID, String email, String username, String password, String role, String inviteCode) {
+        this.UID = UID;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.inviteCode = inviteCode;
+    }
+
+    public RegisterUser() {
+    }
+
+    public int getUID() {
+        return UID;
+    }
+
+    public void setUID(int UID) {
+        this.UID = UID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
 }
