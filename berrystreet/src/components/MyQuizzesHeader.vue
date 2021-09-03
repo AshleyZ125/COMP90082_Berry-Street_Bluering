b4fa1158aebb6f4a2789be89525cded6089f03d3
Ying-da <yingda@student.unimelb.edu.au>
2021-09-03 19:00:16 +1000

    MyQuizzesHeader updated

--- berrystreet/src/components/MyQuizzesHeader.vue
+++ berrystreet/src/components/MyQuizzesHeader.vue
@@ -2,7 +2,7 @@
     <div class = "myQuizzesHeader"> 
         <img src = "./../scss/lexm.png" alt = "icon picture">
         <span class = "myQuizzesText"> My Quizzes </span>
-        <el-button @click="logout()" style = "float:right;margin-right:25px;margin-top:25px;"> Logout </el-button>
+        <el-button @click="logIn()" style = "float:right;margin-right:25px;margin-top:25px;"> Log in </el-button>
     </div>
 </template>
 
@@ -10,8 +10,8 @@
 export default{
     name:'myQuizzes-header',
     methods:{
-        logout(){
-            alert("logout");
+        logIn(){
+            alert("logIn");
         }
     }
 }
