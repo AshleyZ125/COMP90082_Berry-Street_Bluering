45e20603ee6d96cd7656bf65bf1d5d8c6b0fa843
Ying-da <yingda@student.unimelb.edu.au>
2021-09-03 16:18:21 +1000

    createQuiz and MyQuizzes header updated

---
+++ berrystreet/src/components/CreateQuizHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class = "createQuizHeader"> 
+        <img src = "./../scss/lexm.png" alt = "icon picture">
+        <span class = "createQuizText"> Create Quiz </span>
+        <el-button @click="logout()" style = "float:right;margin-right:25px;margin-top:25px;"> Logout </el-button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'createQuiz-header',
+    methods:{
+        logout(){
+            alert("logout");
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
+   .createQuizHeader{
+        height: 90px;
+        line-height: 90px;
+        background-color: #50A7C2;
+        text-align: center;
+        position: relative;
+
+        .img{
+            width: 190px;
+            height: 40px;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 10px;
+            margin: auto;
+        }
+        .createQuizText{
+            font-size: 45px;
+            font-family: 'Acme', sans-serif;  
+            color: #F8F8FF;
+        }
+   }
+
+</style>
