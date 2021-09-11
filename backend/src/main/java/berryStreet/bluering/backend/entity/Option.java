05a14c2ba375b35a39284fa7013d0418e53c1db4
AshleyZ125 <zixinzhang1@student.unimelb.edu.au>
2021-09-11 22:18:48 +0800

    0911 supervisor complete: show list;get/set quiz and question

--- backend/src/main/java/berryStreet/bluering/backend/entity/Option.java
+++ backend/src/main/java/berryStreet/bluering/backend/entity/Option.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Option {
-    private int key;
+    private long key;
     private String value;
     private int point;
 }
