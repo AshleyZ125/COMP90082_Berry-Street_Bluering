c5d2c6d249adaec8d374dde90cdca12b91d429c9
jiwang5 <88428970+jiwang5@users.noreply.github.com>
2021-09-11 15:53:22 +0800

    Update insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -1,28 +1,14 @@
 insert into project.user values (1, 'superviser1@gmail.com', 'superviser1', 'supervisor1pass', 'supervisor');
 insert into project.user values (2, 'LEC1@gmail.com', 'LEC1', 'LEC1pass', 'LEC');
 
-insert into project.quiz values (1, 'Collaborative Learning', 'Collaborative learning is the eduational approach of using groups to enhance learning through working togather...');
+insert into project.quiz values (1, 'Collaborative Learning', 'Collaborative learning is the eduational approach of using groups to enhance learning through working togather...', 2, 1);
 
-insert into project.question values (1, 'When the group needs suggestions, I...',1);
-insert into project.question values (2, 'When the group needs opinions about something, I...',1);
-
-insert into project.options values (1, 'Do not make suggestions',1, 1);
-insert into project.options values (2, 'Tell the group what to do',2, 1);
-insert into project.options values (3, 'Discuss my suggestions with the group',3, 1);
-insert into project.options values (4, 'Make sure everyone\'s suggestions are heard',5, 1);
-
-insert into project.options values (5, 'Do not give my opinion',1, 2);
-insert into project.options values (6, 'Give my opinion',2, 2);
-insert into project.options values (7, 'Explain my opinion so the group understands',4, 2);
-insert into project.options values (8, 'Give reasons for and against my opinion using evidence',6, 2);
+insert into project.question values (1, 'When the group needs suggestions, I...','"Do not make suggestions":1, "Tell the group what to do":2, "Discuss my suggestions with the group":3, "Make sure everyone\'s suggestions are heard":5',1);
+insert into project.question values (2, 'When the group needs opinions about something, I...','"Do not give my opinion":1, "Give my opinion":2, "Explain my opinion so the group understands":4, "Give reasons for and against my opinion using evidence":6',1);
 
 insert into project.feedback values (1, '5-7','At this level, you join in with...',1);
 insert into project.feedback values (2, '8-12','At this level, ...', 1);
 insert into project.feedback values (3, '13-18','At this level, ...', 1);
-
-
-
-
 
 
 
