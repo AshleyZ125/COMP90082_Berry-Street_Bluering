fc146b1afc9df415186e0ba517767177cd4666a7
AshleyZ125 <zixinzhang1@student.unimelb.edu.au>
2021-09-11 20:57:20 +0800

    0911 update: option object update

--- backend/src/main/java/berryStreet/bluering/backend/entity/Option.java
+++ backend/src/main/java/berryStreet/bluering/backend/entity/Option.java
@@ -1,10 +1,14 @@
 package berryStreet.bluering.backend.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Option {
     private int key;
     private String value;
